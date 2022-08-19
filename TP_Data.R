11b102508f2e2511652d4d01f903533aeb627f3e
Jason Doll <jason.doll@fmarion.edu>
2022-08-19 13:37:17 -0400

    1)	Added an abiotic by cohort year table 2)	Created a new stan fine “yep_fie_covar_V2.stan” a.	Calculate the scaled abiotic by year in b.	In generated quantities, added an if statement. If it’s the first cohort year don’t do anything and assign s, p, prev_p, and m = 0. Calculate p_prev based on TP and GDD from the previous year. 3)	I also changed the TP_Data.R file to use “Year” instead of “YEAR” headers to be consistent with GDD and modified the necessary code in the PMRN R file handle this change.

--- TP_Data.R
+++ TP_Data.R
@@ -3,7 +3,7 @@ library(dplyr)
 #Read Data
 TP <- read.csv("SouthernLM_TP.csv")
 
-mean_TP = TP %>%
-  group_by(YEAR) %>%
+mean_TP_byYear = TP %>%
+  group_by(Year) %>%
   dplyr::summarize(Mean = mean(TP_ug_L, na.rm=TRUE), n=n())
 
